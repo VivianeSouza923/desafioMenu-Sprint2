7e5039940d991bd2050224d3e9d5ee8278bfd671
VivianeSouza923 <viviane.souza05@aluno.ifce.edu.br>
2023-07-13 18:19:14 -0300

    today's work

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
 
+
 void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key});
 
   @override
   Widget build(BuildContext context) {
@@ -17,29 +18,93 @@ class MyApp extends StatelessWidget {
 }
 
 class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+  const HomePage({Key? key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: LayoutBuilder( // FERRAMENTA ÚTIL PARA CRIAÇÃO DE DESIGN RESPONSIVO
-        builder: (BuildContext context, BoxConstraints constraints) {
-        return Container(
-          width: constraints.maxWidth, // é para ser 390px, segundo o figma (guarda essa informação.)
-          height: 75,
-          color: const Color(0xffC7411B),
-          child: const Text(
+      appBar: AppBar(
+        toolbarHeight: 75, // Defina a altura desejada aqui
+        backgroundColor: const Color(0xffC7411B),
+        title: const Center(
+          child: Text(
             "Cardápio Web",
             style: TextStyle(
+              fontFamily: 'FigTree',
               fontSize: 24,
               color: Color(0xffFFFFFF),
               fontWeight: FontWeight.w600,
-              
-
             ),
           ),
-        );
-      }
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 23.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const SizedBox(height: 26),
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const Text(
+                  //textAlign: TextAlign.start,
+                  
+                  "Olá, boa noite!",
+                  
+                  style: TextStyle(
+                    
+                    fontFamily: 'FigTree',
+                    fontWeight: FontWeight.w500,
+                    fontSize: 24,
+                    color: Color(0xffC7411B),
+                  ),
+                ),
+                const SizedBox(width: 129),
+                Container(
+                  width: 58,
+                  height: 56,
+                  decoration: BoxDecoration(
+                    color: const Color(0xffC7411B),
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                  child: Transform.rotate(
+                    angle: 3.14159 / 2, // Rotação de 90 graus (pi / 2 radianos)
+                    child: const Icon(
+                      Icons.search,
+                      color: Color(0xffFFFBF6),
+                      size: 30.5,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            
+            
+            const SizedBox(height: 4,),
+
+            
+             const Text(
+              
+              
+              
+              "Cardápio",
+              style: TextStyle(
+                fontFamily: 'FigTree',
+                fontWeight: FontWeight.w400,
+                fontSize: 20,
+                color: Color(0xffC7411B),
+              ),
+             
+              ),
+
+            const SizedBox(height: 16,),
+
+            
+
+            
+          ],
+        ),
       ),
     );
   }
