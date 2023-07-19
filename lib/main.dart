492833b9d7347056ec21639357eabdedc23e4c15
Vivi <viviane.souza05@aluno.ifce.edu.br>
2023-07-19 17:05:25 -0300

    today's work

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_icons/flutter_icons.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key});
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -17,7 +18,7 @@ class MyApp extends StatelessWidget {
 }
 
 class HomePage extends StatelessWidget {
-  const HomePage({Key? key});
+  const HomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -39,166 +40,249 @@ class HomePage extends StatelessWidget {
       ),
       body: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 0.0),
-        child: SingleChildScrollView(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              const SizedBox(height: 26),
-              Row(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  const Padding(
-                    padding: EdgeInsets.only(left: 23.0),
-                    child: Text(
-                      "Olá, boa noite!",
-                      style: TextStyle(
-                        fontFamily: 'FigTree',
-                        fontWeight: FontWeight.w500,
-                        fontSize: 24,
-                        color: Color(0xffC7411B),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const SizedBox(height: 26),
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const Padding(
+                  padding: EdgeInsets.only(left: 23.0),
+                  child: Text(
+                    "Olá, boa noite!",
+                    style: TextStyle(
+                      fontFamily: 'FigTree',
+                      fontWeight: FontWeight.w500,
+                      fontSize: 24,
+                      color: Color(0xffC7411B),
+                    ),
+                  ),
+                ),
+                const Spacer(),
+                Padding(
+                  padding: const EdgeInsets.only(right: 23.0),
+                  child: Container(
+                    width: 58,
+                    height: 56,
+                    decoration: BoxDecoration(
+                      color: const Color(0xffC7411B),
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                    child: Transform.rotate(
+                      angle: 3.14159 / 2,
+                      child: const Icon(
+                        Icons.search,
+                        color: Color(0xffFFFBF6),
+                        size: 30.5,
                       ),
                     ),
                   ),
-                  const Spacer(),
-                  Padding(
-                    padding: const EdgeInsets.only(right: 23.0),
-                    child: Container(
-                      width: 58,
-                      height: 56,
-                      decoration: BoxDecoration(
-                        color: const Color(0xffC7411B),
-                        borderRadius: BorderRadius.circular(20),
-                      ),
-                      child: Transform.rotate(
-                        angle: 3.14159 / 2,
-                        child: const Icon(
-                          Icons.search,
+                ),
+              ],
+            ),
+            const SizedBox(height: 4),
+            const Padding(
+              padding: EdgeInsets.only(left: 23.0),
+              child: Text(
+                "Cardápio",
+                style: TextStyle(
+                  fontFamily: 'FigTree',
+                  fontWeight: FontWeight.w400,
+                  fontSize: 20,
+                  color: Color(0xffC7411B),
+                ),
+              ),
+            ),
+            const SizedBox(height: 16),
+
+            // CARROSSEL DE TIPOS DE REFEIÇÕES - APENAS UMA LISTVIEW
+            Container(
+              height: 56,
+              width: 575, // Defina a altura desejada para a ListView
+              margin: const EdgeInsets.only(left: 23.0),
+              child: ListView(
+                scrollDirection: Axis.horizontal,
+                children: <Widget>[
+                  Container(
+                    width: 131,
+                    height: 56,
+                    decoration: BoxDecoration(
+                      color: const Color(0xffC7411B),
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                    child: const Padding(
+                      padding:
+                          EdgeInsets.symmetric(vertical: 16, horizontal: 37.5),
+                      child: Text(
+                        "Todos",
+                        style: TextStyle(
+                          fontFamily: 'FigTree',
+                          fontWeight: FontWeight.w500,
+                          fontSize: 20,
                           color: Color(0xffFFFBF6),
-                          size: 30.5,
+                        ),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(
+                    width: 17,
+                  ),
+                  Container(
+                    width: 131,
+                    height: 56,
+                    decoration: BoxDecoration(
+                      color: const Color(0xffFFB987),
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                    child: const Padding(
+                      padding:
+                          EdgeInsets.symmetric(vertical: 16, horizontal: 27),
+                      child: Text(
+                        "Petiscos",
+                        style: TextStyle(
+                          fontFamily: 'FigTree',
+                          fontWeight: FontWeight.w400,
+                          fontSize: 20,
+                          color: Color(0xffC7411B),
+                        ),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(
+                    width: 17,
+                  ),
+                  Container(
+                    width: 131,
+                    height: 56,
+                    decoration: BoxDecoration(
+                      color: const Color(0xffFFB987),
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                    child: const Padding(
+                      padding:
+                          EdgeInsets.symmetric(vertical: 17.0, horizontal: 15),
+                      child: Text(
+                        "Sobremesas",
+                        style: TextStyle(
+                          fontFamily: 'FigTree',
+                          fontWeight: FontWeight.w400,
+                          fontSize: 18,
+                          color: Color(0xffC7411B),
                         ),
                       ),
                     ),
                   ),
                 ],
               ),
-              const SizedBox(height: 4),
-              const Padding(
-                padding: EdgeInsets.only(left: 23.0),
-                child: Text(
-                  "Cardápio",
-                  style: TextStyle(
-                    fontFamily: 'FigTree',
-                    fontWeight: FontWeight.w400,
-                    fontSize: 20,
-                    color: Color(0xffC7411B),
-                  ),
+            ),
+
+            const SizedBox(
+              height: 30,
+            ),
+
+            const Padding(
+              padding: EdgeInsets.only(left: 23.0),
+              child: Text(
+                "Todos",
+                style: TextStyle(
+                  fontFamily: 'FigTree',
+                  fontSize: 20,
+                  fontWeight: FontWeight.w400,
+                  color: Color(0xffC7411B),
                 ),
               ),
-              const SizedBox(height: 16),
+            ),
 
-              // CARROSSEL DE TIPOS DE REFEIÇÕES - APENAS UMA LISTVIEW
-              Container(
-                height: 56,
-                width: 575, // Defina a altura desejada para a ListView
-                margin: const EdgeInsets.only(left: 23.0),
-                child: ListView(
-                  scrollDirection: Axis.horizontal,
-                  children: <Widget>[
+            const SizedBox(
+              height: 13,
+            ),
+
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 36.0),
+              child: Container(
+                width: 344,
+                height: 156,
+                decoration: BoxDecoration(
+                  color: const Color(0xffFFEFDC),
+                  borderRadius: BorderRadius.circular(20),
+                ),
+                child: Row(
+                  children: [
                     Container(
-                      width: 131,
-                      height: 56,
+                      margin: const EdgeInsets.only(left: 8, top: 8, bottom: 8),
+                      width: 144,
+                      height: 140,
                       decoration: BoxDecoration(
-                        color: const Color(0xffC7411B),
-                        borderRadius: BorderRadius.circular(20),
+                        color: const Color(0xffFFDCBD),
+                        borderRadius: BorderRadius.circular(12),
                       ),
                       child: const Padding(
-                        padding: EdgeInsets.symmetric(
-                            vertical: 16, horizontal: 37.5),
-                        child: Text(
-                          "Todos",
-                          style: TextStyle(
-                            fontFamily: 'FigTree',
-                            fontWeight: FontWeight.w500,
-                            fontSize: 20,
-                            color: Color(0xffFFFBF6),
-                          ),
+                        padding: EdgeInsets.only(
+                            left: 117.0, top: 8, right: 8, bottom: 113),
+                        child: Icon(
+                          Icons.circle,
+                          size: 19,
+                          color: Color(0xffE45E3A),
                         ),
                       ),
                     ),
                     const SizedBox(
-                      width: 17,
+                      width: 11,
                     ),
-                    Container(
-                      width: 131,
-                      height: 56,
-                      decoration: BoxDecoration(
-                        color: const Color(0xffFFB987),
-                        borderRadius: BorderRadius.circular(20),
-                      ),
-                      child: const Padding(
-                        padding:
-                            EdgeInsets.symmetric(vertical: 16, horizontal: 27),
-                        child: Text(
-                          "Petiscos",
-                          style: TextStyle(
-                            fontFamily: 'FigTree',
-                            fontWeight: FontWeight.w400,
-                            fontSize: 20,
-                            color: Color(0xffC7411B),
-                          ),
+                    const Padding(
+                      padding: EdgeInsets.only(top: 12, bottom: 125),
+                      child: Text(
+                        "001",
+                        style: TextStyle(
+                          fontFamily: 'FigTree',
+                          fontSize: 16,
+                          fontWeight: FontWeight.w400,
+                          color: Color(0xffC7411B),
                         ),
                       ),
                     ),
                     const SizedBox(
-                      width: 17,
+                      width: 89,
                     ),
-                    Container(
-                      width: 131,
-                      height: 56,
-                      decoration: BoxDecoration(
-                        color: const Color(0xffFFB987),
-                        borderRadius: BorderRadius.circular(20),
-                      ),
-                      child: const Padding(
-                        padding: EdgeInsets.symmetric(
-                            vertical: 17.0, horizontal: 15),
-                        child: Text(
-                          "Sobremesas",
-                          style: TextStyle(
-                            fontFamily: 'FigTree',
-                            fontWeight: FontWeight.w400,
-                            fontSize: 18,
-                            color: Color(0xffC7411B),
-                          ),
-                        ),
-                      ),
-                    ),
+                    Icon(),
                   ],
                 ),
               ),
+            ),
 
-              const SizedBox(
-                height: 30,
-              ),
+            const SizedBox(
+              height: 9,
+            ),
 
-              const Padding(
-                padding: EdgeInsets.only(left: 23.0),
-                child: Text(
-                  "Todos",
-                  style: TextStyle(
-                    fontFamily: 'FigTree',
-                    fontSize: 20,
-                    fontWeight: FontWeight.w400,
-                    color: Color(0xffC7411B),
-                  ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 36),
+              child: Container(
+                width: 344,
+                height: 156,
+                decoration: BoxDecoration(
+                  color: const Color(0xffFFEFDC),
+                  borderRadius: BorderRadius.circular(20),
                 ),
               ),
+            ),
 
-              ListView(
-                children: [],
+            const SizedBox(
+              height: 9,
+            ),
+
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 36.0),
+              child: Container(
+                width: 344,
+                height: 156,
+                decoration: BoxDecoration(
+                  color: const Color(0xffFFEFDC),
+                  borderRadius: BorderRadius.circular(20),
+                ),
               ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
