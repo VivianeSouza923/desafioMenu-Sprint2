a58766723f92503808c2c32fe822a91ebbd40967
VivianeSouza923 <viviane.souza05@aluno.ifce.edu.br>
2023-08-16 21:40:43 -0300

    today's work

--- lib/pages/restaurante.dart
+++ lib/pages/restaurante.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 import 'dart:io';
+import 'dart:math';
 
 class Restaurant extends StatelessWidget {
   const Restaurant({Key? key}) : super(key: key);
@@ -18,6 +19,7 @@ class RestaurantScreen extends StatefulWidget {
    const RestaurantScreen({Key? key}) : super(key: key);
 
   @override
+  // ignore: library_private_types_in_public_api
   _RestaurantScreenState createState() => _RestaurantScreenState();
 }
 
@@ -178,66 +180,59 @@ categoriasController.addListener(() {
                     ),
                   ),
                 ),
-                Padding(
-                  padding: const EdgeInsets.only(top: 16.0, left: 24, right: 24),
-                  child: Container(
-                    width: 342,
-                    height: 40,
-                    decoration: BoxDecoration(
-                      color: const Color(0xffFFEFDC),
-                      borderRadius: BorderRadius.circular(10),
-                    ),
-                    child: Padding(
-                      padding:  EdgeInsets.only(top: 14.0, left: showNomeIcon ? 40.0 : -44.0,),
-                      child: TextField(
-                      
-                        
-                      
-                        style: const TextStyle(
-                          fontFamily: 'FigTree',
-                          fontWeight: FontWeight.w400,
-                          fontSize: 16,
-                          color: Color(0xffC7411B),
-                      
-                          
-                      
-                          
-                        ),
-                      
-                        
-                        
-                        controller: nomeController,
-                        // dentro do inputDecoration é que a gente coloca o íconezinho que irá aparecer ao lado do nome da comida
-                        decoration:  InputDecoration(
-                          
-                          prefixIcon: Visibility(
-                            visible: showNomeIcon,
-                            child: Padding(
-                              padding: const EdgeInsets.only(left: 11.0,),
-                              child: const Icon(
-                                Icons.restaurant_menu,
-                                size: 14,
-                                color: Color(0xffFFB987),
-                              ),
-                            ),
-                          ),
-                          hintText: "Nome",
-                          
-                          hintStyle: const TextStyle(
-                            
-                            fontFamily: 'FigTree',
-                            fontSize: 16,
-                            fontWeight: FontWeight.w400,
-                            color: Color(0xffFFB987),
-                          ),
-                    
-                          
-                          border: InputBorder.none,
-                        ),
-                      ),
-                    ),
-                  ),
-                ),
+Padding(
+  padding: const EdgeInsets.only(top: 16.0, left: 24, right: 24),
+  child: Container(
+    width: 342,
+    height: 40,
+    decoration: BoxDecoration(
+      color: const Color(0xffFFEFDC),
+      borderRadius: BorderRadius.circular(10),
+    ),
+    child: Row(
+      children: [
+        Padding(
+          padding: const EdgeInsets.only(left: 12),
+          child: Visibility(
+            visible: showNomeIcon,
+            child: const Icon(
+              Icons.restaurant_menu,
+              size: 14,
+              color: Color(0xffFFB987),
+            ),
+          ),
+        ),
+        const SizedBox(width: 8), // Espaçamento entre o ícone e o TextField
+        Expanded(
+          child: TextField(
+            style: const TextStyle(
+              fontFamily: 'FigTree',
+              fontWeight: FontWeight.w400,
+              fontSize: 16,
+              color: Color(0xffC7411B),
+            ),
+            controller: nomeController,
+            decoration: const InputDecoration(
+              hintText: "Nome",
+              hintStyle: TextStyle(
+                fontFamily: 'FigTree',
+                fontSize: 16,
+                fontWeight: FontWeight.w400,
+                color: Color(0xffFFB987),
+              ),
+              border: InputBorder.none,
+              contentPadding: EdgeInsets.only(left: -4), // Ajuste o valor conforme necessário
+            ),
+          ),
+        ),
+      ],
+    ),
+  ),
+),
+
+
+
+
 
           Padding(
             padding: const EdgeInsets.only(top: 16.0, left: 24, right: 24),
@@ -248,33 +243,44 @@ categoriasController.addListener(() {
                 color: const Color(0xffFFEFDC),
                 borderRadius: BorderRadius.circular(10),
               ),
-              child: Padding(
-                padding: const EdgeInsets.only(top: 14.0, left: 12),
-                child: TextField(
-
-                  style: TextStyle(
-
-                  ),
-                  // pegar o texto que o usuário digitou:
-                  controller: codigoController,
-              
-                  // costumização do campo de texto
-                  decoration: const InputDecoration(
-                    hintText: "Código",
-                    hintStyle: TextStyle(
-                      fontFamily: 'FigTree',
-                      fontSize: 16,
-                      fontWeight: FontWeight.w400,
-                      color: Color(0xffFFB987),
-              
-                    
-              
-                    ),
-              
-                    border: InputBorder.none,
-                  ),
-                ),
+              child: Row(
+      children: [
+        Padding(
+          padding: const EdgeInsets.only(left: 12),
+          child: Visibility(
+            visible: showCodigoIcon,
+            child: const Icon(
+              Icons.tag,
+              size: 14,
+              color: Color(0xffFFB987),
+            ),
+          ),
+        ),
+        const SizedBox(width: 8), // Espaçamento entre o ícone e o TextField
+        Expanded(
+          child: TextField(
+            style: const TextStyle(
+              fontFamily: 'FigTree',
+              fontWeight: FontWeight.w400,
+              fontSize: 16,
+              color: Color(0xffC7411B),
+            ),
+            controller: codigoController,
+            decoration: const InputDecoration(
+              hintText: "Código",
+              hintStyle: TextStyle(
+                fontFamily: 'FigTree',
+                fontSize: 16,
+                fontWeight: FontWeight.w400,
+                color: Color(0xffFFB987),
               ),
+              border: InputBorder.none,
+              contentPadding: EdgeInsets.only(left: -4), // Ajuste o valor conforme necessário
+            ),
+          ),
+        ),
+      ],
+    ),
             ),
           ),
 
@@ -288,29 +294,44 @@ categoriasController.addListener(() {
                 color: const Color(0xffFFEFDC),
                 borderRadius: BorderRadius.circular(10),
               ),
-              child: Padding(
-                padding: const EdgeInsets.only(top: 14.0, left: 12),
-                child: TextField(
-                  // pegar o texto que o usuário digitou:
-                  controller: precoController,
-              
-                  // costumização do campo de texto
-                  decoration: const InputDecoration(
-                    hintText: "Preço",
-                    hintStyle: TextStyle(
-                      fontFamily: 'FigTree',
-                      fontSize: 16,
-                      fontWeight: FontWeight.w400,
-                      color: Color(0xffFFB987),
-              
-                    
-              
-                    ),
-              
-                    border: InputBorder.none,
-                  ),
-                ),
+              child: Row(
+      children: [
+        Padding(
+          padding: const EdgeInsets.only(left: 12),
+          child: Visibility(
+            visible: showPrecoIcon,
+            child: const Icon(
+              Icons.tag,
+              size: 14,
+              color: Color(0xffFFB987),
+            ),
+          ),
+        ),
+        const SizedBox(width: 8), // Espaçamento entre o ícone e o TextField
+        Expanded(
+          child: TextField(
+            style: const TextStyle(
+              fontFamily: 'FigTree',
+              fontWeight: FontWeight.w400,
+              fontSize: 16,
+              color: Color(0xffC7411B),
+            ),
+            controller: precoController,
+            decoration: const InputDecoration(
+              hintText: "Preço",
+              hintStyle: TextStyle(
+                fontFamily: 'FigTree',
+                fontSize: 16,
+                fontWeight: FontWeight.w400,
+                color: Color(0xffFFB987),
               ),
+              border: InputBorder.none,
+              contentPadding: EdgeInsets.only(left: -4), // Ajuste o valor conforme necessário
+            ),
+          ),
+        ),
+      ],
+    ),
             ),
           ),
 
@@ -324,29 +345,45 @@ categoriasController.addListener(() {
                 color: const Color(0xffFFEFDC),
                 borderRadius: BorderRadius.circular(10),
               ),
-              child: Padding(
-                padding: const EdgeInsets.only(top: 15.0, left: 12),
-                child: TextField(
-                  // pegar o texto que o usuário digitou:
-                  controller: descricaoController,
-              
-                  // costumização do campo de texto
-                  decoration: const InputDecoration(
-                    hintText: "Descrição",
-                    hintStyle: TextStyle(
-                      fontFamily: 'FigTree',
-                      fontSize: 16,
-                      fontWeight: FontWeight.w400,
-                      color: Color(0xffFFB987),
-              
-                    
-              
-                    ),
-              
-                    border: InputBorder.none,
-                  ),
-                ),
+               child: Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Padding(
+          padding: const EdgeInsets.only(top: 15.0, left: 12),
+          child: Visibility(
+            visible: showDescricaoIcon,
+            child: const Icon(
+              Icons.notes,
+              size: 14,
+              color: Color(0xffFFB987),
+            ),
+          ),
+        ),
+        const SizedBox(width: 8), // Espaçamento entre o ícone e o TextField
+        Expanded(
+          child: TextField(
+            style: const TextStyle(
+              fontFamily: 'FigTree',
+              fontWeight: FontWeight.w400,
+              fontSize: 14,
+              color: Color(0xffC7411B),
+            ),
+            controller: descricaoController,
+            decoration: const InputDecoration(
+              hintText: "Descrição",
+              hintStyle: TextStyle(
+                fontFamily: 'FigTree',
+                fontSize: 16,
+                fontWeight: FontWeight.w400,
+                color: Color(0xffFFB987),
               ),
+              border: InputBorder.none,
+              contentPadding: EdgeInsets.only(left: -4), // Ajuste o valor conforme necessário
+            ),
+          ),
+        ),
+      ],
+    ),
             ),
           ),
 
@@ -359,29 +396,47 @@ categoriasController.addListener(() {
                 color: const Color(0xffFFEFDC),
                 borderRadius: BorderRadius.circular(10),
               ),
-              child: Padding(
-                padding: const EdgeInsets.only(top: 14.0, left: 12),
-                child: TextField(
-                  // pegar o texto que o usuário digitou:
-                  controller: categoriasController,
-              
-                  // costumização do campo de texto
-                  decoration: const InputDecoration(
-                    hintText: "Categoria(s)",
-                    hintStyle: TextStyle(
-                      fontFamily: 'FigTree',
-                      fontSize: 16,
-                      fontWeight: FontWeight.w400,
-                      color: Color(0xffFFB987),
-              
-                    
-              
-                    ),
-              
-                    border: InputBorder.none,
-                  ),
-                ),
+              child: Row(
+      children: [
+        Padding(
+          padding: const EdgeInsets.only(left: 12),
+          child: Visibility(
+            visible: showCategoriasIcon,
+            child: Transform.rotate(
+              angle: -pi/2,
+              child: const Icon(
+                Icons.sell_outlined,
+                size: 17,
+                color: Color(0xffFFB987),
               ),
+            ),
+          ),
+        ),
+        const SizedBox(width: 8), // Espaçamento entre o ícone e o TextField
+        Expanded(
+          child: TextField(
+            style: const TextStyle(
+              fontFamily: 'FigTree',
+              fontWeight: FontWeight.w400,
+              fontSize: 14,
+              color: Color(0xffC7411B),
+            ),
+            controller: categoriasController,
+            decoration: const InputDecoration(
+              hintText: "Categoria(s)",
+              hintStyle: TextStyle(
+                fontFamily: 'FigTree',
+                fontSize: 16,
+                fontWeight: FontWeight.w400,
+                color: Color(0xffFFB987),
+              ),
+              border: InputBorder.none,
+              contentPadding: EdgeInsets.only(left: -4), // Ajuste o valor conforme necessário
+            ),
+          ),
+        ),
+      ],
+    ),
             ),
           ),
 
